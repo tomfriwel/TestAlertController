33cffd10a30a8beab76c95a39e430c45ad4e35e2
tomfriwel <976932447@qq.com>
2017-05-12 11:04:18 +0800

    Initial Commit

---
+++ TestAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAlertController
+//
+//  Created by tomfriwel on 12/05/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAlertController
+//
+//  Created by tomfriwel on 12/05/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
